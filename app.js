9a55147d1d37aadc76945f522503743ce21b7211
Jeremiah Lateef <jeremiahlat@gmail.com>
2023-03-01 23:50:34 +0100

    added selectize API

--- app.js
+++ app.js
@@ -30,3 +30,71 @@ if (navigator.geolocation)
     },
     function () {}
   );
+
+// Seletivize.js API
+var $select = $("#select-tools").selectize({
+  maxItems: null,
+  valueField: "id",
+  labelField: "title",
+  searchField: "title",
+  options: [
+    {
+      id: 1,
+      title: "Ikeja",
+      url: "http://en.wikipedia.org/wiki/Spectrometers",
+    },
+    {
+      id: 2,
+      title: "Lekki",
+      url: "http://en.wikipedia.org/wiki/Star_chart",
+    },
+    {
+      id: 3,
+      title: "Festac",
+      url: "http://en.wikipedia.org/wiki/Electrical_tape",
+    },
+    {
+      id: 4,
+      title: "Yaba",
+      url: "http://en.wikipedia.org/wiki/Electrical_tape",
+    },
+  ],
+  create: false,
+});
+
+// API controls
+
+var control = $select[0].selectize;
+
+$("#button-clear").on("click", function () {
+  control.clear();
+});
+
+$("#button-clearoptions").on("click", function () {
+  control.clearOptions();
+});
+
+$("#button-addoption").on("click", function () {
+  control.addOption({
+    id: 5,
+    title: "Something New",
+    url: "http://google.com",
+  });
+});
+
+$("#button-additem").on("click", function () {
+  control.addItem(2);
+});
+
+$("#button-maxitems2").on("click", function () {
+  control.setMaxItems(1);
+});
+
+$("#button-maxitems100").on("click", function () {
+  control.setMaxItems(100);
+});
+
+$("#button-setvalue").on("click", function () {
+  control.setValue([2, 3]);
+});
+// dropdown
