41db6bab9e41e74d904e2a04dd60d4d332d1e6f3
Jeremiah Lateef <jeremiahlat@gmail.com>
2023-03-07 02:06:22 +0100

    added animation for the sections

--- app.js
+++ app.js
@@ -17,84 +17,140 @@ if (navigator.geolocation)
       map.on("click", function (mapEvent) {
         const { lat, lng } = mapEvent.latlng;
         L.marker([lat, lng]).addTo(map).bindPopup("Apartment").openPopup();
-      });
 
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(L.popup(closeOnClick, false))
-        .openPopup();
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(L.popup(autoClose, false))
-        .openPopup();
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(L.popup(closeOnClick, false))
+          .openPopup();
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(L.popup(autoClose, false))
+          .openPopup();
+      });
     },
     function () {}
   );
 
 // Seletivize.js API
-var $select = $("#select-tools").selectize({
-  maxItems: null,
-  valueField: "id",
-  labelField: "title",
-  searchField: "title",
-  options: [
-    {
-      id: 1,
-      title: "Ikeja",
-      url: "http://en.wikipedia.org/wiki/Spectrometers",
-    },
-    {
-      id: 2,
-      title: "Lekki",
-      url: "http://en.wikipedia.org/wiki/Star_chart",
-    },
-    {
-      id: 3,
-      title: "Festac",
-      url: "http://en.wikipedia.org/wiki/Electrical_tape",
-    },
-    {
-      id: 4,
-      title: "Yaba",
-      url: "http://en.wikipedia.org/wiki/Electrical_tape",
-    },
-  ],
-  create: false,
-});
+// var $select = $("#select-tools").selectize({
+//   maxItems: null,
+//   valueField: "id",
+//   labelField: "title",
+//   searchField: "title",
+//   options: [
+//     {
+//       id: 1,
+//       title: "Ikeja",
+//       url: "http://en.wikipedia.org/wiki/Spectrometers",
+//     },
+//     {
+//       id: 2,
+//       title: "Lekki",
+//       url: "http://en.wikipedia.org/wiki/Star_chart",
+//     },
+//     {
+//       id: 3,
+//       title: "Festac",
+//       url: "http://en.wikipedia.org/wiki/Electrical_tape",
+//     },
+//     {
+//       id: 4,
+//       title: "Yaba",
+//       url: "http://en.wikipedia.org/wiki/Electrical_tape",
+//     },
+//   ],
+//   create: false,
+// });
 
-// API controls
+// // API controls
 
-var control = $select[0].selectize;
+// var control = $select[0].selectize;
 
-$("#button-clear").on("click", function () {
-  control.clear();
-});
+// $("#button-clear").on("click", function () {
+//   control.clear();
+// });
 
-$("#button-clearoptions").on("click", function () {
-  control.clearOptions();
-});
+// $("#button-clearoptions").on("click", function () {
+//   control.clearOptions();
+// });
 
-$("#button-addoption").on("click", function () {
-  control.addOption({
-    id: 5,
-    title: "Something New",
-    url: "http://google.com",
+// $("#button-addoption").on("click", function () {
+//   control.addOption({
+//     id: 5,
+//     title: "Something New",
+//     url: "http://google.com",
+//   });
+// });
+
+// $("#button-additem").on("click", function () {
+//   control.addItem(2);
+// });
+
+// $("#button-maxitems2").on("click", function () {
+//   control.setMaxItems(1);
+// });
+
+// $("#button-maxitems100").on("click", function () {
+//   control.setMaxItems(100);
+// });
+
+// $("#button-setvalue").on("click", function () {
+//   control.setValue([2, 3]);
+// });
+// dropdown
+
+// // Reveal sections
+// const allSections = document.querySelectorAll(".section");
+
+// const revealSection = function (entries, observer) {
+//   const [entry] = entries;
+
+//   if (!entry.isIntersecting) return;
+
+//   entry.target.classList.remove("section--hidden");
+//   observer.unobserve(entry.target);
+// };
+
+// const sectionObserver = new IntersectionObserver(revealSection, {
+//   root: null,
+//   threshold: 0.15,
+// });
+
+// allSections.forEach(function (section) {
+//   sectionObserver.observe(section);
+//   section.classList.add("section--hidden");
+// });
+
+//Adding the Intersection observer API
+const hidden = document.querySelectorAll(".section--hidden");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
   });
 });
 
-$("#button-additem").on("click", function () {
-  control.addItem(2);
-});
+hidden.forEach((el) => observer.observe(el));
 
-$("#button-maxitems2").on("click", function () {
-  control.setMaxItems(1);
-});
+// Sticky Navigation
+const navbar = document.querySelector(".navbar");
+const navHeight = navbar.getBoundingClientRect().height;
 
-$("#button-maxitems100").on("click", function () {
-  control.setMaxItems(100);
-});
+const stickyNav = function (entries) {
+  const [entry] = entries;
 
-$("#button-setvalue").on("click", function () {
-  control.setValue([2, 3]);
+  if (!entry.isIntersecting) navbar.classList.add("sticky");
+  else navbar.classList.remove("sticky");
+};
+
+const navObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `${navHeight}px`,
 });
-// dropdown
+// navObserver.observe(navbar);
+// console.log(navHeight);
